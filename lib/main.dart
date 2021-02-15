437c2af85ec5b04e3a20c1f5e88845736525bddf
juyeonha <sea@herolaw.kr>
2021-02-15 18:27:10 +0900

    2021.2.15 main.dart진행중

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,38 @@
 import 'package:flutter/material.dart';
+//플루터는 widge으로 앱의 화면을 구성한다.
 
-void main() {
+void main(){
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(,
-      title: 'Flutter Demo',
+class MyApp extends StatelessWidget{ //StatelessWidget 변경되지 않는 , 위젯 하위클래스들 : Icon,IconButton,Text
+  @override //tatelessWidget을 상속하는 Widget은 , "Widget build(BuildContext context)" 를 오버라이딩 해야한다.
+  Widget build(BuildCOntext context){ //각각의 위젯마다 고유의 BuildContext 가 존재
+    return MaterialApp( //MaterialApp은 Theme(테마)와 Route(이동)을 담당
+      //Text 위젯 , Dropdown 버튼  위젯 , AppBar 위젯 , Scaffold 위젯 , ListView 위젯 , StatelessWidget , StatefulWidget , IconButton 위젯 , TextField 위젯 , Padding 위젯 , ThemeData 위젯
+    title:'WinWin Kanban', //appbar 에 보여지는 타이
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: Color.fromRGBO(58, 66, 86, 1.0), //앱바 컬러색깔
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}///////////////
+     // home: ProjectListPage(),
+      onGenerateRoute: onGenerateRoute,
+      );
+    }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
-  final String title;
+   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    //dyamic 명시적으로 타입이 예상되지 않도록 선언이 가능하다.
+     final parts =settings.name.split('?');
+     final args =parts.length == 2 ? Uri.splitQueryString(parts[1]) :null;
+     switch (parts[0]){
+       case'/join':
+         //return MaterialPageRoute(settings: settings, builder: (_)=> JoinPage())
+     }
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
   }
 }
+
+
+
